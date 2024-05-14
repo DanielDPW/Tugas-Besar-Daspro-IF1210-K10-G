68836c7bd2242b55bd7bbd9afdbfe9f59626302b
DanielDPW <150114531+DanielDPW@users.noreply.github.com>
2024-05-14 18:43:51 +0700

    fix:fixed load and save not correctly handling error

--- src/load.py
+++ src/load.py
@@ -8,15 +8,18 @@ def load():
     argparser.add_argument('nama_folder', help='Nama folder tempat data tersimpan')
     args = argparser.parse_args()
     directory = args.nama_folder
-    
-    if os.path.isdir(directory):
-        print("Selamat datang di program OWCA!")
-        user_data = csv_parser.parse_csv(os.path.join(directory, 'user.csv'))
-        monster_data = csv_parser.parse_csv(os.path.join(directory, 'monster.csv'))
-        monster_shop_data = csv_parser.parse_csv(os.path.join(directory, 'monster_shop.csv'))
-        monster_inventory_data = csv_parser.parse_csv(os.path.join(directory, 'monster_inventory.csv'))
-        item_shop_data = csv_parser.parse_csv(os.path.join(directory, 'item_shop.csv'))
-        item_inventory_data = csv_parser.parse_csv(os.path.join(directory, 'item_inventory.csv'))
-        return user_data, monster_data, monster_shop_data, monster_inventory_data, item_shop_data, item_inventory_data
+    if directory is None:
+        print("Tidak ada nama folder yang diberikan")
     else:
-        return [], [], [], [], [], []
+        if os.path.isdir(directory):
+            print("Selamat datang di program OWCA!")
+            user_data = csv_parser.parse_csv(directory + '/user.csv')
+            monster_data = csv_parser.parse_csv(directory +'/monster.csv')
+            monster_shop_data = csv_parser.parse_csv(directory + '/monster_shop.csv')
+            monster_inventory_data = csv_parser.parse_csv(directory + '/monster_inventory.csv')
+            item_shop_data = csv_parser.parse_csv(directory + '/item_shop.csv')
+            item_inventory_data = csv_parser.parse_csv(directory + '/item_inventory.csv')
+            return user_data, monster_data, monster_shop_data, monster_inventory_data, item_shop_data, item_inventory_data
+        else:
+            print(f"Folder {directory} tidak ditemukan")
+            return [], [], [], [], [], []
--- src/save.py
+++ src/save.py
@@ -6,12 +6,12 @@ def save(user_data, monster_data, monster_shop_data, monster_inventory_data, ite
     directory = input("Masukkan nama folder penyimpanan: ")
 
     if os.path.isdir(directory):
-        csv_parser.generate_csv(user_data, os.path.join(directory, 'user.csv'))
-        csv_parser.generate_csv(monster_data, os.path.join(directory, 'monster.csv'))
-        csv_parser.generate_csv(monster_shop_data, os.path.join(directory, 'monster_shop.csv'))
-        csv_parser.generate_csv(monster_inventory_data, os.path.join(directory, 'monster_inventory.csv'))
-        csv_parser.generate_csv(item_shop_data, os.path.join(directory, 'item_shop.csv'))
-        csv_parser.generate_csv(item_inventory_data, os.path.join(directory, 'item_inventory.csv'))
+        csv_parser.generate_csv(user_data, directory +'/user.csv')
+        csv_parser.generate_csv(monster_data, directory + '/monster.csv')
+        csv_parser.generate_csv(monster_shop_data, directory +'/monster_shop.csv')
+        csv_parser.generate_csv(monster_inventory_data, directory + '/monster_inventory.csv')
+        csv_parser.generate_csv(item_shop_data, directory + '/item_shop.csv')
+        csv_parser.generate_csv(item_inventory_data, directory + '/item_inventory.csv')
     else:
         os.mkdir(directory)
         print(f"Folder tidak ditemukan sehingga dibuat folder pada {directory}")
